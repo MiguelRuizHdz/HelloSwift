3b180d32ab46e18dcab5cceeaa1f147e32a14151
MigueRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-20 12:00:06 -0500

    first commit

---
+++ HolaSwift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HolaSwift
+//
+//  Created by Miguel Angel Ruiz on 20/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Swift!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 8")
+    }
+}
---
+++ HolaSwift/HolaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaSwiftApp.swift
+//  HolaSwift
+//
+//  Created by Miguel Angel Ruiz on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HolaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
