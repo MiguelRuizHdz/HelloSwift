062753d29ac6af0d07d9ec85c531f95e64124682
MigueRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-20 13:34:39 -0500

    Estructura de ContentView

--- HolaSwift/ContentView.swift
+++ HolaSwift/ContentView.swift
@@ -8,15 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Funciones y variables se escriben fuera del body
+    
+    var x = "Miguel"
+    
+    // Dentro del body solamente escribir código de diseño
     var body: some View {
-        Text("Hello, Swift!")
+        ContentView2()
+    }
+}
+
+// Se puede tener varias ContentViews y llamarlas desde otra
+struct ContentView2: View {
+    var x = "Miguel"
+    var body: some View {
+        Text("Hello, " + x + "!")
             .font(.largeTitle)
             .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone SE (3rd generation)")
+            ContentView()
+                .previewDevice("iPhone 12 Pro Max")
+        }
     }
 }
